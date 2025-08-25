1ec23e9c7ff9895d6280f48010922d767fe74a8f
Shivam Kagra <shivamkagra38@gmail.com>
2025-08-25 21:02:10 +0530

    Added Welcome Text - Basic

--- Components/SearchBar.js
+++ Components/SearchBar.js
@@ -18,7 +18,7 @@ const SearchBar = () => {
 
   console.log(r);
   const[inputField, setInputField] = useState("");
-  const[weather, setWeather] = useState({status : -1});
+  const[weather, setWeather] = useState({cod : -1});
   const[loading, setLoading] = useState("");
 
   //Updating input field
@@ -60,11 +60,7 @@ const SearchBar = () => {
       let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`);
       let data = await response.json();
 
-      if(data.cod != 200)
-      {
-        //Invalid city
-        //return;
-      }
+      console.log(data);
 
       console.log(data);
       setWeather(data);
@@ -84,7 +80,7 @@ const SearchBar = () => {
   <main className={weather.name != undefined ?  weather.main.temp >= 20 ? "warm" : "cold"  : "warm"}>
 
     <div className="search-container">
-        <input className="search-bar" type="text" placeholder="Type Here..." value={inputField} onChange={inputFunction} ></input>
+        <input className="search-bar" type="text" placeholder="Search City Name Here..." value={inputField} onChange={inputFunction} ></input>
         <button className="search-btn" onClick={searchButtonClicked}>Search</button>
     </div>
     <h3>
@@ -109,7 +105,12 @@ const SearchBar = () => {
         {weather.weather[0].main}
       </div>
 
-    </div> : ""
+    </div> :
+
+    <div className="result-container-initial">
+      <div className="welcome-message-1">Welcome to</div>
+      <div className="welcome-message-2">SumBeam App</div>
+    </div>
 
     }
 
